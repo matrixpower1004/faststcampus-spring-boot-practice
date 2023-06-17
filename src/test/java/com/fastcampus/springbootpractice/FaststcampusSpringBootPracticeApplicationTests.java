6e3b3d3aac279120a0d8c64b7214656139cf9605
matrixpower1004 <matrixpower1004@gmail.com>
2023-06-17 19:25:15 +0900

    Delete #9 - 불필요한 요소 정리
    이전 연습에서 사용했고, 이번 연습의 관심사가 아닌 내용들을 삭제 및 코드 주석 처리

--- src/test/java/com/fastcampus/springbootpractice/FaststcampusSpringBootPracticeApplicationTests.java
+++ src/test/java/com/fastcampus/springbootpractice/FaststcampusSpringBootPracticeApplicationTests.java
@@ -1,23 +1,13 @@
 package com.fastcampus.springbootpractice;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
 
 @SpringBootTest
 class FaststcampusSpringBootPracticeApplicationTests {
 
-    @Autowired
-    private ObjectMapper mapper;
-
     @Test
-    void contextLoads() throws JsonProcessingException {
-        Date date = new Date();
-        System.out.println(mapper.writeValueAsString(date));
+    void contextLoads() {
     }
 
 }
